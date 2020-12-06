9f7fe07f2d9dc5a38c0cd44eb52b5c9e88c2f008
Lawlez <dk.feger@gmail.com>
2020-12-06 16:53:31 +0100

    fixes post request

--- htb/challanges/web/emdee five for life/emdeefiveforlife.js
+++ htb/challanges/web/emdee five for life/emdeefiveforlife.js
@@ -1,4 +1,3 @@
-
 const crypto = require("crypto");
 const http = require("http");
 const axios = require("axios").default;
@@ -6,30 +5,41 @@ const httpAgent = new http.Agent({ keepAlive: true });
 
 const loadData = async () => {
   try {
-    const response = await axios.get("http://144.126.206.242:32609",{httpAgent});
-    console.log(response.data);
+    const response = await axios.get("http://144.126.206.242:32609", {
+      httpAgent,
+    });
+
     const PAGE = response.data;
-    var x = "<h3 align='center'>";
-    var y = "</h3>";
+    const x = "<h3 align='center'>";
+    const y = "</h3>";
     const string = PAGE.substring(
       PAGE.indexOf(x) + x.length,
       PAGE.lastIndexOf(y)
-    )
-console.log('string',string)
-    const md5 = (data) => crypto.createHash("md5").update(string).digest("hex");
- const hash = md5(string)
-console.log('MD%',hash);
+    );
 
-const response2 = await axios({
-			method: 'post', 
-			url: "http://144.126.206.242:32609",
-			headers:{'Content-Type':'application/x-www-form-urlencoded',
-				'Cookie':'PHPSESSID=ilimcjsspls1p1qpnhlbdbf6o6'}, 
-			body:`hash=${hash}`, 
-			httpAgent});
+    console.log("recieved string: ", string);
+    const md5 = (data) => crypto.createHash("md5").update(data).digest("hex");
+    const hash = md5(string);
+    console.log("new MD: ", hash);
 
-console.log(response2.data)
- } catch (error) {
+    const response2 = await axios({
+      method: "post",
+      url: "http://127.0.0.1:32609",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Cookie: "PHPSESSID=ilimcjsspls1p1qpnhlbdbf6o6",
+      },
+      data: `hash=${hash}`,
+      //go through burp proxy
+      proxy: {
+        host: "127.0.0.1",
+        port: 8080,
+      },
+      httpAgent,
+    });
+
+    console.log(response2.data);
+  } catch (error) {
     console.error(error);
   }
 };
