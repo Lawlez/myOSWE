241c0996b7c68bd3370082c6494e26597a42892d
0x0000005 <lwlx@lwlx.xyz>
2020-12-06 10:30:30 -0500

    adds post, tested

--- htb/challanges/web/emdee five for life/emdeefiveforlife.js
+++ htb/challanges/web/emdee five for life/emdeefiveforlife.js
@@ -1,21 +1,35 @@
-var crypto = require("crypto");
+
+const crypto = require("crypto");
+const http = require("http");
 const axios = require("axios").default;
+const httpAgent = new http.Agent({ keepAlive: true });
 
 const loadData = async () => {
   try {
-    const response = await axios.get("https://github.com/axios/axios");
+    const response = await axios.get("http://144.126.206.242:32609",{httpAgent});
     console.log(response.data);
     const PAGE = response.data;
-    var x = "<h3>";
+    var x = "<h3 align='center'>";
     var y = "</h3>";
     const string = PAGE.substring(
       PAGE.indexOf(x) + x.length,
       PAGE.lastIndexOf(y)
-    );
-    const md5 = (data) => crypto.createHash("md5").update(data).digest("hex");
+    )
+console.log('string',string)
+    const md5 = (data) => crypto.createHash("md5").update(string).digest("hex");
+ const hash = md5(string)
+console.log('MD%',hash);
 
-    console.log(md5(string));
-  } catch (error) {
+const response2 = await axios({
+			method: 'post', 
+			url: "http://144.126.206.242:32609",
+			headers:{'Content-Type':'application/x-www-form-urlencoded',
+				'Cookie':'PHPSESSID=ilimcjsspls1p1qpnhlbdbf6o6'}, 
+			body:`hash=${hash}`, 
+			httpAgent});
+
+console.log(response2.data)
+ } catch (error) {
     console.error(error);
   }
 };
