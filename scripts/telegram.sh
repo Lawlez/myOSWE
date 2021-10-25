127391f79f922260a4f86bce9868d45587b96f73
Lawlez <dk.feger@gmail.com>
2021-10-25 15:06:40 +0200

    formatting

---
+++ scripts/telegram.sh
@@ -0,0 +1,3 @@
+curl -s -X POST https://api.telegram.org/bot2057680969:AAEDDDDDDDDDDDDDDDDDDDDDDDDDDDAl4Ic/sendMessage -d chat_id="-2303920930" -d text="hi"
+
+curl -s -X POST https://api.telegram.org/bot2057680969:AAEDDDDDDDDDDDDDDDDDDDDDDDDDDDAl4Ic/getUpdates
