6a9f2c8483115afbf1e6bd09b727b2847b1eae5c
Lawlez <dk.feger@gmail.com>
2024-02-08 16:46:52 +0100

    updates fetchsourcemap to optimize loading of multiple maps

--- scripts/fetchSourceMap.php
+++ scripts/fetchSourceMap.php
@@ -1,23 +1,38 @@
 <?php
 
-$file = 'compat.umd.js.map';
+$directoryPath = '/maps'; // Pfad zum Durchsuchen
+$outputDirectory = '../output'; // Zielverzeichnis für die extrahierten Dateien
+$pattern = '/\.js\.map$/'; // Regulärer Ausdruck für Dateien, die auf .js.map enden
 
-if(file_exists($file)) {
-    $json = json_decode(file_get_contents($file));
+if (!is_dir($outputDirectory)) {
+    mkdir($outputDirectory, 0755, true); // Erstelle das Zielverzeichnis, falls es nicht existiert
+}
 
-    $directories = [];
+$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directoryPath));
+$jsMapFiles = [];
 
-    foreach ($json->sources as $index => $source) {
-
-        $dir = dirname($source);
-        if (!is_dir($dir)) {
-            mkdir($dir, 0755, true);
-        }
-        $data = explode('/', $source);
-        $file = end($data);
-        file_put_contents($dir . "/" . $file, $json->sourcesContent[$index]);
-        $files[] = $dir . "/" . $file;
-
+foreach ($files as $file) {
+    if ($file->isFile() && preg_match($pattern, $file->getFilename())) {
+        $jsMapFiles[] = $file->getPathname();
     }
-    print_r($files);
-} 
+}
+
+foreach ($jsMapFiles as $file) {
+    if (file_exists($file)) {
+        $json = json_decode(file_get_contents($file));
+        $directories = [];
+
+        foreach ($json->sources as $index => $source) {
+            $dir = $outputDirectory . '/' . dirname($source);
+            if (!is_dir($dir)) {
+                mkdir($dir, 0755, true);
+            }
+            $data = explode('/', $source);
+            $filename = end($data);
+            file_put_contents($dir . "/" . $filename, $json->sourcesContent[$index]);
+            $directories[] = $dir . "/" . $filename;
+        }
+        print_r($directories);
+    }
+}
+?>
