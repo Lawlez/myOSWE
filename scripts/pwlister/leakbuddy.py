c42cd616cb880c4854cabbadbf0dcda607f0879d
Dominik Feger <dk.feger@gmail.com>
2024-02-24 14:36:43 +0100

    fix encoding errors

--- scripts/pwlister/leakbuddy.py
+++ scripts/pwlister/leakbuddy.py
@@ -6,8 +6,6 @@ def ensure_directory_exists(directory):
         os.makedirs(directory)
 
 def user_confirm_overwrite(file_path):
-    # This is a placeholder for user confirmation logic
-    # Adapt this part based on your actual requirements
     return input(f'The file {file_path} already exists. Overwrite? (y/n): ').lower() == 'y'
 
 def remove_duplicates(strings):
@@ -16,12 +14,26 @@ def remove_duplicates(strings):
     duplicates_removed = len(strings) - len(set(strings))
     return [x for x in strings if not (x in seen or seen_add(x))], duplicates_removed
 
+def process_extracted_file(file_name, all_filtered_strings):
+    try:
+        # Attempt to open the file with UTF-8 encoding
+        with open(file_name, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+    except UnicodeDecodeError:
+        # If a UnicodeDecodeError occurs, try ISO-8859-1 encoding
+        with open(file_name, 'r', encoding='iso-8859-1') as file:
+            lines = file.readlines()
+
+    # Extract and store the desired part of each line
+    filtered_strings = [line.split(':')[-1].strip() for line in lines]
+    all_filtered_strings.extend(filtered_strings)
+
+    return len(lines)  # Return the number of processed lines for logging
+
 def unzip_and_filter_files(output_file_name):
     ensure_directory_exists('./out')
-    
     output_file_path = f'./out/{output_file_name}'
     
-    # Check if the output file exists and confirm overwrite if necessary
     if os.path.exists(output_file_path) and not user_confirm_overwrite(output_file_path):
         print("Operation cancelled by user.")
         return
@@ -35,15 +47,12 @@ def unzip_and_filter_files(output_file_name):
                 zip_ref.extractall('.')
                 
                 for extracted_file in zip_ref.namelist():
-                    with open(extracted_file, 'r') as file:
-                        lines = file.readlines()
-                        filtered_strings = [line.split(':')[-1].strip() for line in lines]
-                        all_filtered_strings.extend(filtered_strings)
-                        total_processed_lines += len(lines)
+                    processed_lines = process_extracted_file(extracted_file, all_filtered_strings)
+                    total_processed_lines += processed_lines
                     
                     os.remove(extracted_file)
 
-    # Remove duplicates from the list
+    # Remove duplicates and count them
     unique_strings, duplicates_removed = remove_duplicates(all_filtered_strings)
 
     # Write the unique strings to the file
